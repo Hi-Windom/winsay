f0f8a8ef1f478bb94d1fd495adfb49e0fd40ed6c
Soltus <liaoshanyi@qq.com>
2023-03-30 18:51:56 +0800

    6

---
+++ index-903b3b20.js
@@ -0,0 +1,33 @@
+/*!
+*                    .::::.
+*                  .::::::::.
+*                 :::::::::::  winsay v2.0.16
+*             ..:::::::::::'
+*           '::::::::::::'
+*             .::::::::::
+*        '::::::::::::::..
+*             ..::::::::::::.
+*           .''::::::::::::::::
+*            ::::'':::::::::'        .:::.
+*           ::::'   ':::::'       .::::::::.
+*         .::::'      ::::     .:::::::'::::.
+*        .:::'       :::::  .:::::::::' ':::::.
+*       .::'        :::::.:::::::::'      ':::::.
+*      .::'         ::::::::::::::'         ''::::.
+*  ...:::           ::::::::::::'              ''::.
+* '''' ':.          ':::::::::'                  ::::..
+*                    '.:::::'                    ':'''''..
+*
+ *
+ * https://github.com/Hi-Windom/winsay
+ * QQ 694357845
+ *
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ *
+ */
+import{i as a,l as i,k as n}from"./localforage-b8b4c264.js";import{a as s,C as t,b as o,c as e,d as w,e as c,f as d,g as r,h as l,i as u,j as y}from"./index-849c0692.js";function _(){async function _(){s(),await async function(){const a=await i.getItem("SC_winsay_cp_custom-nav-bind-id");n(a)||(document.getElementById(a).checked=!0);document.querySelectorAll("input[id^='sc-custom-nav-bind-id'][type='radio']").forEach((async a=>{a.addEventListener("click",(async()=>{a.checked&&!0===a.checked&&await i.setItem("SC_winsay_cp_custom-nav-bind-id",a.id)}))}))}(),await async function(){await e(),await w(),await c(),await d(),await r(),await l(),await u(),await y()}(),await t(),await o()}a()&&(require("fs"),require("path")),window.winsay.localVersion={},window.winsay.localVersion.version="",window.winsay.localVersion.useBazaar="",window.winsay.localVersion.useGithub="",window.winsay.localVersion.vinfoHTML="",window.winsay.storage.SC_winsay_cp_editor__DocWidthMode__previousValue="null",window.winsay.storage.SC_winsay_cp_T__t2=null,window.winsay.storage.It_DocWidthMode=null,window.winsay.storage.It_SelfProtector=null,window.winsay.storage.It_filterTimer=null,setTimeout((async()=>{await _()}),0)}export{_ as CP};
