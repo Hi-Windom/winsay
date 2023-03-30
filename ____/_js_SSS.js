f0f3c1e0b48c2dd45fdc768a6f91d06f6f183f5e
Soltus <liaoshanyi@qq.com>
2023-03-30 18:23:55 +0800

    6

--- ____/_js_SSS.js
+++ ____/_js_SSS.js
@@ -1,7 +1,7 @@
 /*!
 *                    .::::.
 *                  .::::::::.
-*                 :::::::::::  winsay v2.0.13
+*                 :::::::::::  winsay v2.0.14
 *             ..:::::::::::'
 *           '::::::::::::'
 *             .::::::::::
@@ -30,4 +30,4 @@
  * never gonna say goodbye. Never gonna tell a lie and hurt you.
  *
  */
-import{i as s,m as t,r as a,u as o,c as i,S as e,n as l,o as r}from"../localforage-223dc0ab.js";import{iterLC as n,iterDC as c}from"../switchLD-2e8e8b28.js";import{M as m,D as f}from"../desktop-f3c53f50.js";async function d(){if("dark"===t)await c();else await n();"body--mobile"===i?(await async function(){if("body--mobile"===i){var s=document.getElementById("SofillToolbar");if(null==s){var t=document.getElementById("toolbarEdit");(s=r(t,"div","SofillToolbar")).style.position="relative",s.style.height="25px",s.style.overflowY="scroll",s.style.paddingTop="7px",s.style.marginRight="3px",s.style.marginLeft="10px"}}}(),await m()):await f()}s()&&require("fs"),setTimeout((()=>{!async function(){let s=null;"light"===t?(s=`${e}root-base-light.css`,a(`${e}root-base-dark.css`,"css")):(s=`${e}root-base-dark.css`,a(`${e}root-base-light.css`,"css"));setTimeout((()=>{o(l,s)}),100),await d()}()}),0);
+import{i as s,m as t,r as a,u as o,c as i,S as e,n as l,o as r}from"../localforage-0b3895d5.js";import{iterLC as n,iterDC as c}from"../switchLD-1b40d944.js";import{M as m,D as f}from"../desktop-1a63ead5.js";async function d(){if("dark"===t)await c();else await n();"body--mobile"===i?(await async function(){if("body--mobile"===i){var s=document.getElementById("SofillToolbar");if(null==s){var t=document.getElementById("toolbarEdit");(s=r(t,"div","SofillToolbar")).style.position="relative",s.style.height="25px",s.style.overflowY="scroll",s.style.paddingTop="7px",s.style.marginRight="3px",s.style.marginLeft="10px"}}}(),await m()):await f()}s()&&require("fs"),setTimeout((()=>{!async function(){let s=null;"light"===t?(s=`${e}root-base-light.css`,a(`${e}root-base-dark.css`,"css")):(s=`${e}root-base-dark.css`,a(`${e}root-base-light.css`,"css"));setTimeout((()=>{o(l,s)}),100),await d()}()}),0);
