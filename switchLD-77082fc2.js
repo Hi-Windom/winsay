eabe1a060604fb2db3d0b370dd6a7dcc83dfe4d0
Soltus <liaoshanyi@qq.com>
2023-03-30 19:00:20 +0800

    6

--- switchLD-77082fc2.js
+++ switchLD-77082fc2.js
@@ -1,7 +1,7 @@
 /*!
 *                    .::::.
 *                  .::::::::.
-*                 :::::::::::  winsay v2.0.16
+*                 :::::::::::  winsay v2.0.17
 *             ..:::::::::::'
 *           '::::::::::::'
 *             .::::::::::
@@ -30,4 +30,4 @@
  * never gonna say goodbye. Never gonna tell a lie and hurt you.
  *
  */
-import{l as a,G as e,k as t,P as n,u as s,S as o,L as r,e as i,F as l,Q as w,d as c}from"./localforage-b8b4c264.js";async function g(){await a.getItem(e).then((a=>{t(a)&&(a=n,console.warn(a)),s(r.STYLE_COLOR,`${o}${a}`);for(let e=0;e<i.length&&window.winsay.storage.iterDC.next().value!==a;++e);}))}async function f(){await a.getItem(l).then((a=>{t(a)&&(a=w,console.warn(a)),s(r.STYLE_COLOR,`${o}${a}`);for(let e=0;e<c.length&&window.winsay.storage.iterLC.next().value!==a;++e);}))}export{g as iterDC,f as iterLC};
+import{l as a,G as e,k as t,P as n,u as s,S as o,L as r,e as i,F as l,Q as w,d as c}from"./localforage-871a1048.js";async function g(){await a.getItem(e).then((a=>{t(a)&&(a=n,console.warn(a)),s(r.STYLE_COLOR,`${o}${a}`);for(let e=0;e<i.length&&window.winsay.storage.iterDC.next().value!==a;++e);}))}async function f(){await a.getItem(l).then((a=>{t(a)&&(a=w,console.warn(a)),s(r.STYLE_COLOR,`${o}${a}`);for(let e=0;e<c.length&&window.winsay.storage.iterLC.next().value!==a;++e);}))}export{g as iterDC,f as iterLC};
